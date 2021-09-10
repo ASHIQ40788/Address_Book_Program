0a7723c1a11ec6726a4dd444ecfab98b702ff4f1
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-10 21:03:39 +0530

    [ASHIQ] added: EDITED_EXISTING_CONTACT

--- AddressBook.cs
+++ AddressBook.cs
@@ -41,5 +41,50 @@ namespace Address_Book
             }
             return false;
         }
+
+        public bool UpdateContact(string originalName)
+        {
+            Console.Write("Are you sure you would you like to update the Contact? -- Type 'Y' or 'N': ");
+            string userResponse = Console.ReadLine().ToLower();
+            if (userResponse == "y")
+            {
+                Console.Write($"Would you like to update {originalName}'s name or address? TYPE - 'Name' for name and 'Address' for address: ");
+                string entryToUpdate = Console.ReadLine().ToLower();
+
+                //, (entryToUpdate == "address") ? "address" : "name"
+                Console.Write($"Please enter changes to the {entryToUpdate} here: ");
+                string userUpdatedEntry = Console.ReadLine();
+
+                int index = GetEntryIndex(originalName);
+                if (entryToUpdate == "name")
+                {
+                    contacts[index].Name = userUpdatedEntry;
+                    Console.WriteLine($"Contact {originalName} updated to {userUpdatedEntry}");
+                    return true;
+                }
+                if (entryToUpdate == "address")
+                {
+                    contacts[index].Address = userUpdatedEntry;
+                    Console.WriteLine($"Contact {originalName}'s {entryToUpdate} updated to {userUpdatedEntry}");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private int GetEntryIndex(string name)
+        {
+            for (int i = 0; i < contacts.Length; i++)
+            {
+                if (contacts[i] != null && contacts[i].Name == name)
+                    return i;
+            }
+            return -1;
+        }
+
+        private bool ContainsEntry(string name)
+        {
+            return GetEntryIndex(name) != -1;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,64 @@ namespace Address_Book
     {
         static void Main(string[] args)
         {
+            AddressBook addressBook = new AddressBook();
+            void UpdateAddressBook(string userInput)
+            {
+                string name = "";
+                string address = "";
+                string city = "";
+                string state = "";
+                switch (userInput.ToLower())
+                {
+                    case "add":
+                        Console.Write("Enter a name: ");
+                        name = Console.ReadLine();
+                        switch (name)
+                        {
+                            case "quit":
+                                break;
+                            default:
+                                Console.Write("Enter an address: ");
+                                address = Console.ReadLine();
+                                switch (address)
+                                {
+                                    case "quit":
+                                        break;
+                                        Console.Write("Enter a city: ");
+                                        city = Console.ReadLine();
+                                        switch (city)
+                                        {
+                                            case "quit":
+                                                break;
+                                            default:
+                                                Console.Write("Enter an state: ");
+                                                state = Console.ReadLine();
+                                                switch (a)
+                                                {
+                                                    case "quit":
+                                                        break;
+                                                    default:
+                                                        addressBook.AddEntry(name, address, city, state);
+                                                        break;
+                                                }
+                                                break;
+                                          
+                                            case "view":
+                                                Console.WriteLine(addressBook.ViewContactsList());
+                                                break;
+                                            case "update":
+                                                Console.WriteLine("Please enter the name of the Contact you wish to update");
+                                                name = Console.ReadLine();
+                                                addressBook.UpdateContact(name);
+                                                break;
+                                        }
+                                }
+
+                        }
+                                
+                        
+                }
+            }
         }
     }
 }
