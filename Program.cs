27a1870e521f13fc24c453d54ed6ee4f8af45473
ASHIQ40788 <86375129+ASHIQ40788@users.noreply.github.com>
2021-09-10 20:45:05 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,48 @@ namespace Address_Book
     {
         static void Main(string[] args)
         {
+             AddressBook addressBook = new AddressBook();
+            void UpdateAddressBook(string userInput)
+            {
+                string name = "";
+                string address = "";
+                string city = "";
+                string state = "";
+                witch ( userInput.ToLower() ) 
+                {
+                    case "add":
+                        Console.Write("Enter a name: ");
+                        name = Console.ReadLine();
+                        switch(name) 
+                        {
+                            case "quit":
+                                break;
+                            default:
+                                Console.Write("Enter an address: ");
+                                address = Console.ReadLine();
+                                switch (address) 
+                                {
+                                    case "quit":
+                                        break;
+                                        Console.Write("Enter a city: ");
+                          city = Console.ReadLine();
+                         switch(city) 
+                        {
+                            case "quit":
+                                break;
+                            default:
+                                Console.Write("Enter an state: ");
+                                state = Console.ReadLine();
+                                switch (a) 
+                                {
+                                    case "quit":
+                                        break;
+                                    default:
+                                        addressBook.AddEntry(name, address, city, state);
+                                        break;
+                                }
+                                break;
+                        }
         }
     }
 }
