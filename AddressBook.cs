ed2219a93c2b5a80719d2c51e2734adafebc627e
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-10 20:28:07 +0530

    [ASHIQ] added: ADDING A NEW_CONTACT

---
+++ AddressBook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Address_Book
+{
+    class AddressBook
+    {
+
+        public readonly Contact[] contacts;
+
+        public AddressBook()
+        {
+            contacts = new Contact[2]; ;
+        }
+
+        public bool AddEntry(string name, string address, string city, string state)
+        {
+            if (!ContainsEntry(name))
+            {
+                Contact AddContact = new Contact(name, address, city, state);
+                for (int i = 0; i < contacts.Length; i++)
+                {
+                    if (contacts[i] == null)
+                    {
+                        contacts[i] = AddContact;
+                        Console.WriteLine("Address Book updated. {0} has been added!", name);
+                        return true;
+                    }
+                }
+                Console.WriteLine($"Cannot add ({name}) to Address Book since it is full!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine($"({name}) already exists in Address Book!");
+                UpdateContact(name);
+            }
+            return false;
+        }
+    }
+}
--- Contact.cs
+++ Contact.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Address_Book
 {
-    class Address
+    class Contact
     {
+        public string name;
+        public string address;
+        public string city;
+        public string state;
+
+        public Contact(string name, string address,string city,string state)
+        {
+            this.name = name;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+        }
     }
 }
+
